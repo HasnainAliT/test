20485e83edd173f7b06eb6a214620195731dca70
Hasnain Talpur <144050177+HasnainAliT@users.noreply.github.com>
2024-10-13 08:13:27 +0000

    understand const,let,var

--- .devcontainer/01_Basics/test.js
+++ .devcontainer/01_Basics/test.js
@@ -1,1 +1,8 @@
-console.log("hasnain");
+const accountId = 12345
+let accountEmail = "ali@123"
+var accountPassword = "chainkulikimain"
+accountCity = "Hyderabad"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountId,accountPassword,accountCity])
